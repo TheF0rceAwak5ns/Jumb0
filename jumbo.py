f8ac4334a9f2f3e1143472bc8d7165f7d5fae6ce
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2023-12-20 13:14:46 +0100

    create - main def with options & mode

--- jumbo.py
+++ jumbo.py
@@ -1,4 +1,5 @@
 import requests
+import argparse
 
 def login(user, password, url):
     urll = url
@@ -12,8 +13,26 @@ def login(user, password, url):
     r = requests.post(url=urll, data=data)
     text = r.text
     print(r.text)
+def main():
+    parser = argparse.ArgumentParser(description='Jumbo loves to put some RCE in CMS')
 
+    # Choose your CMS MODE
+    parser.add_argument('mode', choices=['wordpress', 'joomla'], help='Choose a CMS mode: wordpress or joomla', nargs='?')
 
-login('admin', 'Jumb0%40Ice', 'http://192.168.5.45/wp-login.php')
+    # Options for WordPress & Joomla
+    parser.add_argument('-u', '--username', dest='username', help='username')
+    parser.add_argument('-p', '--password', dest='password', help='password')
+    parser.add_argument('-H', '--url', dest='url', help='CMS website URL')
 
-    
+    args = parser.parse_args()
+
+    if args.mode:
+        if args.mode == 'wordpress':
+            print('Mode: wordpress')
+        elif args.mode == 'joomla':
+            print('Mode: joomla')
+    else:
+        print("No CMS mode specified. Please choose a CMS mode: wordpress or joomla.")
+
+if __name__ == "__main__":
+    main()
